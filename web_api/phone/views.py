7be72a534528e6d77a839c76a71cdef9ede55e4d
TransformersWsz <3287124026@qq.com>
2019-08-02 22:26:58 +0800

    add django web

---
+++ web_api/phone/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello World. You arfsdfsdfe at the phone index")
