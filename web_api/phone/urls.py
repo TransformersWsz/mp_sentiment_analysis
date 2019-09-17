d360f2cff823139bdc5bf7c2bd76ad50c6903f80
TransformersWsz <3287124026@qq.com>
2019-09-17 22:08:42 +0800

    构建了docker镜像

--- web_api/phone/urls.py
+++ web_api/phone/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("list", views.get_phone_list, name="list")
+    path("list", views.get_phone_list, name="list"),
+    path("phones/<int:p_id>", views.get_phone_by_id, name="detail")
 ]
--- web_api/phone/views.py
+++ web_api/phone/views.py
@@ -14,6 +14,7 @@ def index(request):
 
 
 def get_phone_list(request):
+    """分页查询手机"""
     specification_objects = Specification.objects
     if request.GET["search"] == "all":
         items = specification_objects.all()
@@ -21,13 +22,28 @@ def get_phone_list(request):
         items = specification_objects.filter(name__contains=request.GET["search"])
 
     resp = {
-        'code': 200,
+        'code': 3001,
         'msg': 'query success',
         'data': []
     }
     for each_phone in items[int(request.GET["offset"]):int(request.GET["limit"])]:
         d = model_to_dict(each_phone)
         resp["data"].append(d)
+
+    return JsonResponse(resp, safe=False, json_dumps_params={'ensure_ascii': False})
+
+
+def get_phone_by_id(request, p_id):
+    """根据p_id来查询某一款手机的详细信息"""
+    detailed_phone = Specification.objects.filter(p_id=p_id)
+    resp = {}
+    if len(detailed_phone) == 0:
+        resp["code"] = 3002
+        resp["msg"] = "the phone of {} does not exist".format(p_id)
+    else:
+        resp["code"] = 3001
+        resp["msg"] = "query success"
+        resp["data"] = model_to_dict(detailed_phone[0])
 
     return JsonResponse(resp, safe=False, json_dumps_params={'ensure_ascii': False})
 
